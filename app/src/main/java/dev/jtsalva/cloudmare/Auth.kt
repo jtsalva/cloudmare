e41ff3b37b2e48dd9fc4f60d8c70f6a5682fd9f2
Jae Timothy Salva <tim@jtsalva.dev>
2019-07-29 16:59:47 +0100

    changed Auth to an object

--- app/src/main/java/dev/jtsalva/cloudmare/Auth.kt
+++ app/src/main/java/dev/jtsalva/cloudmare/Auth.kt
@@ -6,40 +6,36 @@ import androidx.core.content.edit
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKeys
 
-class Auth {
+object Auth {
+    private const val SHARED_PREFS_FILENAME = "dev.jtsalva.cloudmare.eprefs"
 
-    companion object {
-        private const val SHARED_PREFS_FILENAME = "dev.jtsalva.cloudmare.eprefs"
+    var email = ""
+    var apiKey = ""
 
-        var email = ""
-        var apiKey = ""
+    val isSet: Boolean get() = !notSet
+    val notSet: Boolean get() = email == "" || apiKey == ""
 
-        val isSet: Boolean get() = !notSet
-        val notSet: Boolean get() = email == "" || apiKey == ""
+    private val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
 
-        private val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+    private fun getPrefs(context: Context): SharedPreferences =
+        EncryptedSharedPreferences.create(
+            SHARED_PREFS_FILENAME,
+            masterKeyAlias,
+            context,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
 
-        private fun getPrefs(context: Context): SharedPreferences =
-            EncryptedSharedPreferences.create(
-                SHARED_PREFS_FILENAME,
-                masterKeyAlias,
-                context,
-                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-            )
+    fun load(context: Context) {
+        val preferences = getPrefs(context)
 
-        fun load(context: Context) {
-            val preferences = getPrefs(context)
-
-            email = preferences.getString("email", null) ?: ""
-            apiKey = preferences.getString("api_key", null) ?: ""
-        }
-
-        fun save(context: Context) =
-            getPrefs(context).edit(commit = true) {
-                putString("email", email)
-                putString("api_key", apiKey)
-            }
+        email = preferences.getString("email", null) ?: ""
+        apiKey = preferences.getString("api_key", null) ?: ""
     }
 
+    fun save(context: Context) =
+        getPrefs(context).edit(commit = true) {
+            putString("email", email)
+            putString("api_key", apiKey)
+        }
 }
