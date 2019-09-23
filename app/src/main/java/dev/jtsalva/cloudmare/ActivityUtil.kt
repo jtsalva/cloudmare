cfcf622865b3a12c3c4f977642bd0698e7b7d76c
jtsalva <tim@jtsalva.dev>
2019-09-23 20:23:26 +0100

    inline extensions methods

--- app/src/main/java/dev/jtsalva/cloudmare/ActivityUtil.kt
+++ app/src/main/java/dev/jtsalva/cloudmare/ActivityUtil.kt
@@ -8,21 +8,15 @@ import java.io.InvalidClassException
 import java.security.InvalidParameterException
 import kotlin.reflect.KClass
 
-//fun Context.toast(message: CharSequence) =
-//    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-//
-//fun Context.longToast(message: CharSequence) =
-//    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-
-fun <T : Activity> Context.startActivity(activityClass: KClass<T>) =
+inline fun <T : Activity> Context.startActivity(activityClass: KClass<T>) =
     startActivity(Intent(this, activityClass.java))
 
-fun <T : Activity> Context.startActivityWithExtras(activityClass: KClass<T>, vararg extras: Pair<String, Any>) =
+inline fun <T : Activity> Context.startActivityWithExtras(activityClass: KClass<T>, vararg extras: Pair<String, Any>) =
     startActivity(
         Intent(this, activityClass.java).putExtras(*extras)
     )
 
-fun <T : Activity> Context.startActivityWithExtrasForResult(activityClass: KClass<T>,
+inline fun <T : Activity> Context.startActivityWithExtrasForResult(activityClass: KClass<T>,
                                                             requestCode: Int,
                                                             vararg extras: Pair<String, Any>) =
     if (this is Activity) startActivityForResult(
@@ -30,7 +24,7 @@ fun <T : Activity> Context.startActivityWithExtrasForResult(activityClass: KClas
     ) else throw InvalidClassException("only subclasses of Activity can call this extension function")
 
 
-fun Intent.putExtras(vararg extras: Pair<String, Any>): Intent {
+inline fun Intent.putExtras(vararg extras: Pair<String, Any>): Intent {
     for (pair in extras) when (pair.second) {
         is Int -> putExtra(pair.first, pair.second as Int)
         is String -> putExtra(pair.first, pair.second as String)
