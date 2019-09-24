4740e307c0ccc9dec5a920c4bee50a26416b749b
jtsalva <tim@jtsalva.dev>
2019-09-24 20:05:56 +0100

    display ttl in user friendly format

--- app/src/main/java/dev/jtsalva/cloudmare/api/NumberUtil.kt
+++ app/src/main/java/dev/jtsalva/cloudmare/api/NumberUtil.kt
@@ -2,4 +2,14 @@ package dev.jtsalva.cloudmare.api
 
 import kotlin.math.round
 
-inline val Double.roundedHours: Double get() = round(this / (60 * 60))
+const val MINUTE = 60.0
+const val HOUR = MINUTE * 60.0
+const val DAY = HOUR * 24.0
+
+inline fun Double.toTtlString(): String = when {
+    this < HOUR -> "${this / MINUTE} minutes"
+
+    this < DAY -> "${this / HOUR} hours"
+
+    else -> "${this / DAY} days"
+}
