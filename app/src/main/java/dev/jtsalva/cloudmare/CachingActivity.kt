0ba688ecf29d516fc33ff6a6c361a684f5cb5fbb
jtsalva <tim@jtsalva.dev>
2019-10-14 21:13:10 +0100

    create skeleton for new caching feature

---
+++ app/src/main/java/dev/jtsalva/cloudmare/CachingActivity.kt
@@ -0,0 +1,81 @@
+package dev.jtsalva.cloudmare
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import dev.jtsalva.cloudmare.api.zone.Zone
+import dev.jtsalva.cloudmare.api.zonesettings.ZoneSetting
+import dev.jtsalva.cloudmare.api.zonesettings.ZoneSettingRequest
+import dev.jtsalva.cloudmare.databinding.ActivityCachingBindingImpl
+import dev.jtsalva.cloudmare.viewmodel.CachingViewModel
+import kotlinx.android.synthetic.main.activity_caching.*
+
+class CachingActivity : CloudMareActivity(), SwipeRefreshable {
+
+    private lateinit var domain: Zone
+
+    private lateinit var binding: ActivityCachingBindingImpl
+
+    private lateinit var viewModel: CachingViewModel
+
+    val cacheLevelAdapter by lazy {
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.entries_cache_level,
+            R.layout.spinner_item
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        domain = intent.getParcelableExtra("domain")!!
+
+        viewModel = CachingViewModel(this, domain)
+        binding = setLayoutBinding(R.layout.activity_caching)
+
+        setToolbarTitle("${domain.name} | Caching")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        render()
+    }
+
+    override fun onSwipeRefresh() {
+        super.onSwipeRefresh()
+
+        viewModel.isFinishedInitializing = false
+    }
+
+    override fun render() = launch {
+        val response = ZoneSettingRequest(this).list(domain.id)
+        if (response.failure || response.result == null)
+            dialog.error(message = response.firstErrorMessage, onAcknowledge = ::onStart)
+
+        else response.result.let { settings ->
+            viewModel.cacheLevel = settings.valueAsString(ZoneSetting.ID_CACHE_LEVEL)
+
+            cacheLevelAdapter.let { adapter ->
+                adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+
+                cache_level_spinner.apply {
+                    setAdapter(adapter)
+
+                    val currentCacheLevel = viewModel.run { cacheLevelTranslator.getReadable(cacheLevel) }
+                    setSelection(adapter.getPosition(currentCacheLevel))
+
+                    onItemSelectedListener = viewModel
+                }
+            }
+
+            binding.viewModel = viewModel
+
+            viewModel.isFinishedInitializing = true
+            caching_view_group.visibility = View.VISIBLE
+        }
+
+        showProgressBar = false
+    }
+}
