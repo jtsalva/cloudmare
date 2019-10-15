76c5f57dcaa4910daeb097a2b6e3091d0168e15d
jtsalva <tim@jtsalva.dev>
2019-10-15 11:27:30 +0100

    functioning controls for caching

--- app/src/main/java/dev/jtsalva/cloudmare/ZoneSettingUtil.kt
+++ app/src/main/java/dev/jtsalva/cloudmare/ZoneSettingUtil.kt
@@ -7,5 +7,8 @@ inline fun List<ZoneSetting>.oneWithId(id: String) = find { it.id == id }!!
 inline fun List<ZoneSetting>.valueAsString(id: String) =
     oneWithId(id).value as String
 
+inline fun List<ZoneSetting>.valueAsDouble(id: String) =
+    oneWithId(id).value as Double
+
 inline fun List<ZoneSetting>.valueAsBoolean(id: String) =
     valueAsString(id) == ZoneSetting.VALUE_ON
