3768e50941fa8c51c088003110d974b0a07ba5eb
jtsalva <tim@jtsalva.dev>
2019-10-16 10:17:02 +0100

    add analytics activity

---
+++ app/src/main/java/dev/jtsalva/cloudmare/AnalyticsActivity.kt
@@ -0,0 +1,11 @@
+package dev.jtsalva.cloudmare
+
+import android.os.Bundle
+
+class AnalyticsActivity : CloudMareActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_analytics)
+    }
+}
