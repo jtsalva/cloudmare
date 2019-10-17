d8fadc982964a01b064d02e90380b7c779fbe165
jtsalva <tim@jtsalva.dev>
2019-10-17 21:09:36 +0100

    graphing works with real data

--- app/src/main/java/dev/jtsalva/cloudmare/api/analytics/AnalyticsDashboardRequest.kt
+++ app/src/main/java/dev/jtsalva/cloudmare/api/analytics/AnalyticsDashboardRequest.kt
@@ -3,14 +3,25 @@ package dev.jtsalva.cloudmare.api.analytics
 import dev.jtsalva.cloudmare.CloudMareActivity
 import dev.jtsalva.cloudmare.api.Request
 import dev.jtsalva.cloudmare.api.getAdapter
+import dev.jtsalva.cloudmare.api.toDateString
+import timber.log.Timber
+import java.util.*
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 class AnalyticsDashboardRequest(context: CloudMareActivity) : Request<AnalyticsDashboardRequest>(context) {
 
     suspend fun get(zoneId: String) = suspendCoroutine<AnalyticsDashboardResponse> { cont ->
+        val params = urlParams(
+//            "since" to Date(Calendar.getInstance().time.time - 604800000L).toDateString(),
+//            "until" to Calendar.getInstance().time.toDateString(),
+//            "continuous" to "false"
+        )
+
+        Timber.e(params)
+
         requestTAG = GET
-        get("zones/$zoneId/analytics/dashboard") {
+        get("zones/$zoneId/analytics/dashboard$params") {
             cont.resume(
                 getAdapter(AnalyticsDashboardResponse::class).
                     fromJson(it.toString()) ?: AnalyticsDashboardResponse(success = false)
