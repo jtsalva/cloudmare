08ceb378e0cd3ff1835454f0696fcabd79547113
jtsalva <tim@jtsalva.dev>
2019-10-19 20:32:57 +0100

    remove redundant constants & refactor MutableList to IntArray

--- app/src/main/java/dev/jtsalva/cloudmare/MenuButtonInitializer.kt
+++ app/src/main/java/dev/jtsalva/cloudmare/MenuButtonInitializer.kt
@@ -6,21 +6,15 @@ import android.view.MenuItem
 class MenuButtonInitializer {
     private fun Int.toMenuItem(menu: Menu): MenuItem = menu.findItem(this)
 
-    companion object {
-        const val USER_ACTIVITY_ACTION = R.id.action_user_activity
-        const val SORT_BY_ACTION = R.id.action_sort_by
-        const val SAVE_ACTION = R.id.action_save
-        const val DELETE_ACTION = R.id.action_delete
-        const val ADD_ACTION = R.id.action_add
+    private lateinit var itemsToInflate: IntArray
+
+    fun onInflateSetVisible(vararg buttonIds: Int) {
+        itemsToInflate = buttonIds
     }
 
-    private val itemsToInflate = mutableListOf<Int>()
-
-    fun onInflateSetVisible(vararg buttonIds: Int) =
-        itemsToInflate.addAll(buttonIds.asList())
-
-    fun inflate(menu: Menu) = itemsToInflate.forEach { buttonId ->
-        buttonId.toMenuItem(menu).isVisible = true
+    fun inflate(menu: Menu) {
+        if (::itemsToInflate.isInitialized) itemsToInflate.forEach { buttonId ->
+            buttonId.toMenuItem(menu).isVisible = true
+        }
     }
-
 }
