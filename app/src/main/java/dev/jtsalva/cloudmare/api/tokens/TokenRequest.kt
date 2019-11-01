0229735f045cbad9ef1781c0288741d0651cc564
jtsalva <tim@jtsalva.dev>
2019-11-01 08:51:00 +0000

    add token verification api request

--- app/src/main/java/dev/jtsalva/cloudmare/api/tokens/TokenRequest.kt
+++ app/src/main/java/dev/jtsalva/cloudmare/api/tokens/TokenRequest.kt
@@ -1,2 +1,23 @@
 package dev.jtsalva.cloudmare.api.tokens
 
+import dev.jtsalva.cloudmare.CloudMareActivity
+import dev.jtsalva.cloudmare.api.Request
+import dev.jtsalva.cloudmare.api.Response
+import dev.jtsalva.cloudmare.api.getAdapter
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+class TokenRequest(context: CloudMareActivity) : Request<TokenRequest>(context) {
+
+    suspend fun verify() =
+        suspendCoroutine<Response> { cont ->
+            requestTAG = GET
+            get("/user/tokens/verify") {
+                cont.resume(
+                    getAdapter(Response::class).
+                        fromJson(it.toString()) ?: Response(success = false)
+                )
+            }
+        }
+
+}
