99cfeafdaf08ee3165a9a0e229648f54a69e10a3
jtsalva <tim@jtsalva.dev>
2019-11-02 12:12:19 +0000

    use constant for default time period

--- app/src/main/java/dev/jtsalva/cloudmare/api/analytics/AnalyticsDashboardRequest.kt
+++ app/src/main/java/dev/jtsalva/cloudmare/api/analytics/AnalyticsDashboardRequest.kt
@@ -3,18 +3,19 @@ package dev.jtsalva.cloudmare.api.analytics
 import dev.jtsalva.cloudmare.CloudMareActivity
 import dev.jtsalva.cloudmare.api.Request
 import dev.jtsalva.cloudmare.api.getAdapter
-import timber.log.Timber
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 class AnalyticsDashboardRequest(context: CloudMareActivity) : Request<AnalyticsDashboardRequest>(context) {
 
-    suspend fun get(zoneId: String, since: Int = -10080) = suspendCoroutine<AnalyticsDashboardResponse> { cont ->
+    companion object {
+        private const val SEVEN_DAYS = 10080
+    }
+
+    suspend fun get(zoneId: String, since: Int = -SEVEN_DAYS) = suspendCoroutine<AnalyticsDashboardResponse> { cont ->
         val params = urlParams(
             "since" to since.toString()
         )
-
-        Timber.e(params)
 
         requestTAG = GET
         get("zones/$zoneId/analytics/dashboard$params") {
