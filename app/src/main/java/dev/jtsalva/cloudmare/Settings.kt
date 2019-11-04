1ac3c78914e2255635c658ee25f35f1ef8817eea
jtsalva <tim@jtsalva.dev>
2019-11-04 08:46:06 +0000

    add ability to set app theme

---
+++ app/src/main/java/dev/jtsalva/cloudmare/Settings.kt
@@ -0,0 +1,25 @@
+package dev.jtsalva.cloudmare
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.core.content.edit
+
+object Settings {
+    private const val SHARED_PREFS_FILENAME = "dev.jtsalva.cloudmare.prefs.settings"
+    private const val THEME_PREFS = "theme"
+
+    var theme = MODE_NIGHT_NO
+
+    private fun getPrefs(context: Context): SharedPreferences =
+        context.getSharedPreferences(SHARED_PREFS_FILENAME, Context.MODE_PRIVATE)
+
+    fun load(context: Context) = with (getPrefs(context)) {
+        theme = getInt(THEME_PREFS, theme)
+    }
+
+    fun save(context: Context) =
+        getPrefs(context).edit(commit = true) {
+            putInt(THEME_PREFS, theme)
+        }
+}
