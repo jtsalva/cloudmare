4db594efa87cb4ffcfa36c85b5b732be5d33817b
Tim Salva <tim@jtsalva.dev>
2020-03-03 19:00:33 +0000

    remove unused fields from UserDetails

--- app/src/main/java/dev/jtsalva/cloudmare/api/user/UserDetails.kt
+++ app/src/main/java/dev/jtsalva/cloudmare/api/user/UserDetails.kt
@@ -7,14 +7,5 @@ import com.squareup.moshi.JsonClass
 data class UserDetails(
     @field:Json(name = "id") val id: String,
     @field:Json(name = "email") val email: String,
-    @field:Json(name = "first_name") val firstName: String?,
-    @field:Json(name = "last_name") val lastName: String?,
-    @field:Json(name = "username") val username: String,
-    @field:Json(name = "telephone") val telephone: String?,
-    @field:Json(name = "country") val country: String?,
-    @field:Json(name = "zipcode") val zipcode: String?,
-    @field:Json(name = "created_on") val createdOn: String,
-    @field:Json(name = "modified_on") val modifiedOn: String,
-    @field:Json(name = "two_factor_authentication_enabled") val twoFactorAuthenticationEnabled: Boolean,
     @field:Json(name = "suspended") val suspended: Boolean
 )
